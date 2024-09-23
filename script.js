e237846a6c472887e17de792afd6cd58b4d5f0e5
dani <queirozdaniel66@gmail.com>
2024-09-23 08:46:17 -0300

    "Updated code formatting and removed image with id 6 from the images array."

--- script.js
+++ script.js
@@ -5,21 +5,20 @@ var images = [
   { id: 3, votes: 0, order: 2 },
   { id: 4, votes: 0, order: 3 },
   { id: 5, votes: 0, order: 4 },
-  { id: 6, votes: 0, order: 5 },
 ];
 
 // Função para enviar a ordem das imagens para o servidor
 function saveImageOrderToServer() {
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/save-image-order', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
+  xhr.open("POST", "/save-image-order", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function () {
     if (xhr.status === 200) {
-      console.log('Ordem das imagens salva com sucesso!');
+      console.log("Ordem das imagens salva com sucesso!");
     }
   };
-  xhr.onerror = function() {
-    console.error('Erro ao salvar ordem das imagens:', xhr.statusText);
+  xhr.onerror = function () {
+    console.error("Erro ao salvar ordem das imagens:", xhr.statusText);
   };
   xhr.send(JSON.stringify(images));
 }
@@ -27,8 +26,8 @@ function saveImageOrderToServer() {
 // Função para recuperar a ordem das imagens do servidor
 function loadImageOrderFromServer() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/get-image-order', true);
-  xhr.onload = function() {
+  xhr.open("GET", "/get-image-order", true);
+  xhr.onload = function () {
     if (xhr.status === 200) {
       images = JSON.parse(xhr.responseText);
       updateImageList();
@@ -39,18 +38,18 @@ function loadImageOrderFromServer() {
 
 // Função para atualizar a lista de imagens
 function updateImageList() {
-  var imageList = document.getElementById('image-list');
-  imageList.innerHTML = '';
-  images.forEach(function(image, index) {
-    var imageElement = document.createElement('img');
-    imageElement.src = 'Smolder_' + image.id + '.jpeg';
-    imageElement.alt = 'Imagem ' + image.id;
-    var button = document.createElement('button');
-    button.textContent = 'Votar';
-    button.onclick = function() {
+  var imageList = document.getElementById("image-list");
+  imageList.innerHTML = "";
+  images.forEach(function (image, index) {
+    var imageElement = document.createElement("img");
+    imageElement.src = "Smolder_" + image.id + ".jpeg";
+    imageElement.alt = "Imagem " + image.id;
+    var button = document.createElement("button");
+    button.textContent = "Votar";
+    button.onclick = function () {
       vote(image.id);
     };
-    var listItem = document.createElement('li');
+    var listItem = document.createElement("li");
     listItem.style.order = image.order; // Defina a ordem do item com base na propriedade order
     listItem.appendChild(imageElement);
     listItem.appendChild(button);
@@ -61,23 +60,23 @@ function updateImageList() {
 // Função para votar em uma imagem
 function vote(imageId) {
   // Verifica se o usuário já votou nessa imagem
-  var image = images.find(function(image) {
+  var image = images.find(function (image) {
     return image.id === imageId;
   });
   if (image.votes === 0) {
     image.votes++;
     // Reordena as imagens com base nos votos
-    images.sort(function(a, b) {
+    images.sort(function (a, b) {
       return b.votes - a.votes;
     });
     // Atualiza a ordem das imagens
-    images.forEach(function(image, index) {
+    images.forEach(function (image, index) {
       image.order = index;
     });
     saveImageOrderToServer(); // Envia a ordem das imagens para o servidor
     updateImageList();
   } else {
-    console.log('Você já votou nessa imagem!');
+    console.log("Você já votou nessa imagem!");
   }
 }
 
